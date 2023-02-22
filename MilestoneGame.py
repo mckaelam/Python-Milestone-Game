8628079a7bda18edbbefec99809b423d68ea1907
Mckaela Murry <124202986+mckaelam@users.noreply.github.com>
2023-02-22 09:34:02 -0700

    Create MilestoneGame.py
    first commit for this file.

---
+++ MilestoneGame.py
@@ -0,0 +1,17 @@
+import func
+
+# func.printDelay(func.Intro)
+# print(func.AsciiRat)
+# print (func.Line)
+# func.Wait()
+
+# func.printDelay(func.Instructions)
+# print (func.Line)
+# func.Wait()
+
+# func.printDelay(func.StartPoint)s
+# print (func.Line)
+# func.Choises()
+
+func.Riddle()
+func.CheckInv()
